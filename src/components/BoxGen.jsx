883ab707e5f5f3d78927dfa18ad13d3ba06d3a81
Tyler <tylerwertman@outlook.com>
2023-02-22 22:05:51 -0500

    before major changes to do last challenge

--- src/components/BoxGen.jsx
+++ src/components/BoxGen.jsx
@@ -4,18 +4,29 @@ const BoxGen = () => {
 
     const [newColor, setNewColor] = useState({
         newColor: ""
-    });
+    })
 
     const [colorList, setColorList] = useState([])
     
+    const [heightWidth, setHeightWidth] = useState({
+        hW: ""
+    })
+
+    // const dims = (e) => {
+    //     setHeightWidth({...heightWidth, [e.target.name]: e.target.value})
+    //     console.log(e.target.value)
+    // }
     const newSelectedColor = (e) => {
         setNewColor({...newColor, [e.target.name]: e.target.value})
+        console.log(heightWidth.hW)
     }
 
     const addColorToList = (e) => {
         e.preventDefault();
-        setColorList([...colorList, newColor])
         console.log(colorList)
+        setColorList([...colorList, newColor])
+        setNewColor({newColor: ""})
+        setHeightWidth({heightWidth: ""})
     }
     
     return (
@@ -25,11 +36,24 @@ const BoxGen = () => {
                 <form action="" onSubmit={(addColorToList)}>
                     <label htmlFor="">Type a color: </label>
                     <input type="text" name="newColor" value={newColor.newColor} onChange={newSelectedColor}/>
+                    <input type="number" name="heightWidth" value={heightWidth.hW}/>
                     <button>Submit</button>
                 </form>
             </div>
             <div>
-                
+                {
+                    colorList.map((newColor, i) =>
+                    <div key={i} style={{
+                        display: "inline-block",
+                        width: `${heightWidth.hW}px`,
+                        height: `${heightWidth.hW}px`,
+                        margin: "10px",
+                        backgroundColor: newColor.newColor
+                    }}>
+                        <p>{newColor.newColor}</p>
+                    </div>
+                    )
+                }
             </div>
         </div>
     )
