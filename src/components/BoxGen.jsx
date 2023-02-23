8bcdf6db204f1f8b7f11765f4a20455fb35c00ce
Tyler <tylerwertman@outlook.com>
2023-02-22 22:33:13 -0500

    test

--- src/components/BoxGen.jsx
+++ src/components/BoxGen.jsx
@@ -3,14 +3,14 @@ import React, {useState} from 'react'
 const BoxGen = () => {
 
     const [newColor, setNewColor] = useState({
-        newColor: ""
+        newColor: "",
+        hW: "50"
     })
 
     const [colorList, setColorList] = useState([])
     
-    const [heightWidth, setHeightWidth] = useState({
-        hW: ""
-    })
+    // const [heightWidth, setHeightWidth] = useState({
+    // })
 
     // const dims = (e) => {
     //     setHeightWidth({...heightWidth, [e.target.name]: e.target.value})
@@ -18,7 +18,7 @@ const BoxGen = () => {
     // }
     const newSelectedColor = (e) => {
         setNewColor({...newColor, [e.target.name]: e.target.value})
-        console.log(heightWidth.hW)
+        console.log(newColor.hW)
     }
 
     const addColorToList = (e) => {
@@ -26,7 +26,7 @@ const BoxGen = () => {
         console.log(colorList)
         setColorList([...colorList, newColor])
         setNewColor({newColor: ""})
-        setHeightWidth({heightWidth: ""})
+        setNewColor({hW: ""})
     }
     
     return (
@@ -36,7 +36,7 @@ const BoxGen = () => {
                 <form action="" onSubmit={(addColorToList)}>
                     <label htmlFor="">Type a color: </label>
                     <input type="text" name="newColor" value={newColor.newColor} onChange={newSelectedColor}/>
-                    <input type="number" name="heightWidth" value={heightWidth.hW}/>
+                    <input type="number" name="heightWidth" value={newColor.hW} onChange={newSelectedColor}/>
                     <button>Submit</button>
                 </form>
             </div>
@@ -45,8 +45,8 @@ const BoxGen = () => {
                     colorList.map((newColor, i) =>
                     <div key={i} style={{
                         display: "inline-block",
-                        width: `${heightWidth.hW}px`,
-                        height: `${heightWidth.hW}px`,
+                        width: `${newColor.hW}px`,
+                        height: `${newColor.hW}px`,
                         margin: "10px",
                         backgroundColor: newColor.newColor
                     }}>
