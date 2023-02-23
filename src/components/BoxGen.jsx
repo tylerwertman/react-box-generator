26a08e8fa7d8125c57c0df0471fab90f1762a5f0
Tyler <tylerwertman@outlook.com>
2023-02-22 22:42:46 -0500

    final

--- src/components/BoxGen.jsx
+++ src/components/BoxGen.jsx
@@ -4,18 +4,19 @@ const BoxGen = () => {
 
     const [newColor, setNewColor] = useState({
         newColor: "",
-        hW: "50"
+        hW: ""
     })
 
     const [colorList, setColorList] = useState([])
-    
-    // const [heightWidth, setHeightWidth] = useState({
-    // })
 
-    // const dims = (e) => {
-    //     console.log(e.target.value)
-    // }
+
+    const dims = (e) => {
+        console.log(e.target.value)
+        setNewColor({...newColor, [e.target.name]: e.target.value})
+    }
+
     const newSelectedColor = (e) => {
+        // setNewColor({...newColor.heightWidth, [e.target.name]: e.target.value})
         setNewColor({...newColor, [e.target.name]: e.target.value})
         console.log(newColor.hW)
     }
@@ -24,8 +25,8 @@ const BoxGen = () => {
         e.preventDefault();
         console.log(colorList)
         setColorList([...colorList, newColor])
-        setNewColor({newColor: ""})
-        setNewColor({hW: ""})
+        setNewColor({newColor: "", hW: ""})
+        // setNewColor({hW: ""})
     }
     
     return (
@@ -35,8 +36,7 @@ const BoxGen = () => {
                 <form action="" onSubmit={(addColorToList)}>
                     <label htmlFor="">Type a color: </label>
                     <input type="text" name="newColor" value={newColor.newColor} onChange={newSelectedColor}/>
-                    <input type="number" name="heightWidth" value={newColor.hW} onChange={newSelectedColor}/>
-                    <input type="number" name="heightWidth" value={newColor.hW} onChange={newSelectedColor}/>
+                    <input type="number" name="hW" value={newColor.hW} onChange={dims}/>
                     <button>Submit</button>
                 </form>
             </div>
@@ -45,8 +45,6 @@ const BoxGen = () => {
                     colorList.map((newColor, i) =>
                     <div key={i} style={{
                         display: "inline-block",
-                        width: `${newColor.hW}px`,
-                        height: `${newColor.hW}px`,
                         width: `${newColor.hW}px`,
                         height: `${newColor.hW}px`,
                         margin: "10px",
